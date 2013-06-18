fc1f3e7a72a439416d114cbe170371e92af76aea
Lance Pollard <lancejpollard@gmail.com>
2013-06-18 16:14:28 -0700

    render markdown

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ var md = require('marked');
  * Expose `markdownDirective`.
  */
 
-module.exports = directive('[type="text/markdown"]', markdownDirective);
+//module.exports = directive('[type="text/markdown"]', markdownDirective);
+module.exports = directive('data-markdown', markdownDirective);
 
 /**
  * Define `markdownDirective`.
  */
 
 function markdownDirective(scope, el, attr) {
-  // XXX: todo
-  el.textContent = marked(el.textContent);
+  // XXX: todo, expression
+  var val = attr.value
+    ? scope.get(attr.value)
+    : el.textContent;
+
+  if (val) el.innerHTML = md(val);
 }
